4923581ee843333322b4939f059d30cbc52c60b8
jiangshangzheng <jiangshangzheng@idcmatic.com>
2021-03-26 11:40:26 +0800

    chore(commit 规范): commit 规范

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # vite-vue-app
 vite-vue-app
+
+
+## husky 防止不规范代码被commit、push、merge等
+git commit 的时候，会先执行precommit 的脚
--- commitlint.config.js
+++ commitlint.config.js
@@ -2,20 +2,23 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [
-      2,
-      'always',
-      [
-        'bug', // 此项特别针对bug号，用于向测试反馈bug列表的bug修改情况
-        'feature', // 新功能（feature）
+        2,
+        'always',
+        [
+        'feat', // 新功能（feature）
         'fix', // 修补bug
         'docs', // 文档（documentation）
         'style', // 格式（不影响代码运行的变动）
         'refactor', // 重构（即不是新增功能，也不是修改bug的代码变动）
         'test', // 增加测试
-        'chore', // 构建过程或辅助工具的变动
-        'revert', // feat(pencil): add ‘graphiteWidth’ option (撤销之前的commit)
-        'merge', // 合并分支， 例如： merge（前端页面）： feature-xxxx修改线程地址
-      ],
+        'revert', // 回滚
+        'config', // 构建过程或辅助工具的变动
+        'chore', // 其他改动
+        ],
     ],
-  },
+    'type-empty': [2, 'never'], // 提交不符合规范时,也可以提交,但是会有警告
+    'subject-empty': [2, 'never'], // 提交不符合规范时,也可以提交,但是会有警告
+    'subject-full-stop': [0, 'never'],
+    'subject-case': [0, 'never'],
+  }
 }
